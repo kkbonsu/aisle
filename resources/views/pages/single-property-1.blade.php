547b49463c8035f6ac56420bf76928fbf21aa8f0
Kwame Bonsu <kwamebonsu92@gmail.com>
2022-04-04 01:21:25 +0000

    new8

--- resources/views/pages/single-property-1.blade.php
+++ resources/views/pages/single-property-1.blade.php
@@ -832,170 +832,42 @@
                   </li>
                 </ul>
                 <div class="card-body px-sm-6 shadow-xxs-2 pb-5 pt-0 bg-white">
-                  <form>
+                  {!! Form::open(array('route' => 'buys.store', 'method' => 'POST')) !!}
+                  
                     <div class="tab-content pt-1 pb-0 px-0 shadow-none">
-                      <div class="tab-pane fade show active" id="schedule" role="tabpanel">
-                        <div class="slick-slider calendar arrow-hide-disable mx-n1"
-									     data-slick-options='{"slidesToShow": 5, "autoplay":false,"dots":false}'>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer active border-0 shadow-xxs-1" data-date="March 17, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Tue</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">17</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer border-0 shadow-xxs-1" data-date="March 18, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Wed</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">18</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer border-0 shadow-xxs-1" data-date="March 19, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Thur</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">19</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer border-0 shadow-xxs-1" data-date="March 20, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Fri</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">20</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer border-0 shadow-xxs-1" data-date="March 21, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Sat</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">21</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer border-0 shadow-xxs-1" data-date="March 22, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Sun</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">22</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="box px-1 py-4">
-                            <div class="card pointer border-0 shadow-xxs-1" data-date="March 23, 2020">
-                              <div class="card-body p-1 text-center">
-                                <p class="day fs-12 text-muted lh-2 mb-0">Mon</p>
-                                <p class="date fs-18 text-heading lh-173 mb-0 font-weight-bold">23</p>
-                                <p class="month fs-13 letter-spacing-087 mb-0">Mar</p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <input type="hidden" class="date" name="date" value="March 17, 2020">
-                        <div class="form-group mb-2">
-                          <div class="input-group input-group-lg bootstrap-timepicker timepicker">
-                            <input type="text" class="form-control border-0 text-body shadow-none"
-											       placeholder="Choose a time">
-                            <div class="input-group-append input-group-addon">
-                              <button class="btn bg-input shadow-none fs-18 lh-1" type="button"><i
-														class="fal fa-angle-down"></i></button>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="form-group mb-2">
-                          <input type="text" class="form-control form-control-lg border-0"
-										       placeholder="First Name, Last Name">
-                        </div>
-                        <div class="form-group mb-2">
-                          <input type="email" class="form-control form-control-lg border-0" placeholder="Your Email">
-                        </div>
-                        <div class="form-group mb-4">
-                          <input type="tel" class="form-control form-control-lg border-0" placeholder="Your phone">
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-lg btn-block rounded">Schedule A Tour</button>
-                        <div class="form-group form-check mt-2 mb-0">
-                          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                          <label class="form-check-label fs-13" for="exampleCheck1">Egestas fringilla phasellus
-                            faucibus
-                            scelerisque
-                            eleifend donec.</label>
-                        </div>
-                      </div>
                       <div class="tab-pane fade pt-5" id="request-info" role="tabpanel">
                         <div class="form-check d-flex align-items-center border-bottom pb-3 mb-3">
-                          <input class="form-check-input" type="radio" name="agent" value="option1">
-                          <div class="form-check-label ml-2">
-                            <div class="d-flex align-items-center">
-                              <a href="agent-details-1.html" class="d-block w-60px h-60 mr-3">
-                                <img src="images/agent-2.jpg"
-													     class="rounded-circle" alt="Oliver Beddows">
-                              </a>
-                              <div>
-                                <a href="agent-details-1.html"
-													   class="d-block text-dark font-weight-500 lh-15 hover-primary">Oliver
-                                  Beddows</a>
-                                <p class="mb-0 fs-13 mb-0 lh-17">Sales Excutive</p>
-                                <p class="mb-0 fs-13 mb-0 lh-17"><span>(+123)</span><span
-															class="text-heading d-inline-block ml-2">1900 68668</span>
-                                </p>
-                              </div>
-                            </div>
+                        
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                          <div class="form-group">
+                              <strong>Property Title*:</strong>
+                              <select name="property" class="form-control" id="property">
+                                <option value="{{ $property->name }}">{{ $property->name }}</option>
+                              </select>
                           </div>
                         </div>
-                        <div class="form-check d-flex align-items-center mb-6">
-                          <input class="form-check-input" type="radio" name="agent" id="inlineRadio2"
-										       value="option2">
-                          <div class="form-check-label ml-2">
-                            <div class="d-flex align-items-center">
-                              <a href="agent-details-1.html" class="d-block w-60px h-60 mr-3">
-                                <img src="images/agent-1.jpg"
-													     class="rounded-circle" alt="Max Kordex">
-                              </a>
-                              <div>
-                                <a href="agent-details-1.html"
-													   class="d-block text-dark font-weight-500 lh-15 hover-primary">Max Kordex</a>
-                                <p class="mb-0 fs-13 mb-0 lh-17">Real estate broker</p>
-                                <p class="mb-0 fs-13 mb-0 lh-17"><span>(+123)</span><span
-															class="text-heading d-inline-block ml-2">1900 68668</span>
-                                </p>
-                              </div>
-                            </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                          <div class="form-group">
+                              <strong>Your Name*:</strong>
+                              {!! Form::text('name', null, array('placeholder' => '','class' => 'form-control')) !!}
                           </div>
                         </div>
-                        <div class="form-group mb-2">
-                          <input type="text" class="form-control form-control-lg border-0"
-										       placeholder="First Name, Last Name">
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                          <div class="form-group">
+                              <strong>Your Phone Number*:</strong>
+                              {!! Form::text('phone', null, array('placeholder' => '','class' => 'form-control')) !!}
+                          </div>
                         </div>
-                        <div class="form-group mb-2">
-                          <input type="email" class="form-control form-control-lg border-0" placeholder="Your Email">
-                        </div>
-                        <div class="form-group mb-2">
-                          <input type="tel" class="form-control form-control-lg border-0" placeholder="Your phone">
-                        </div>
-                        <div class="form-group mb-4">
-                          <textarea class="form-control border-0" rows="4">Hello, I'm interested in Villa Called Archangel</textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-lg btn-block rounded">Request Info</button>
-                        <div class="form-group form-check mt-2 mb-0">
-                          <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                          <label class="form-check-label fs-13" for="exampleCheck2">Egestas fringilla phasellus
-                            faucibus
-                            scelerisque
-                            eleifend donec.</label>
-                        </div>
+                      <button type="submit" class="btn btn-primary btn-lg btn-block rounded">Send Buy Request
+                      </button>
                       </div>
                     </div>
-                  </form>
+                {!! Form::close() !!}
+                  @if ($message = Session::get('success'))
+                  <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                  </div>
+                  @endif
                 </div>
               </div>
             </div>
